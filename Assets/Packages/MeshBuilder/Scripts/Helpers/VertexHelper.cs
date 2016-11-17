e13173df4551a48e2200588423e199bdf70eafc2
Masatatsu Nakamura <supa.matz.naka@gmail.com>
2016-11-17 19:19:47 +0900

    first commit.

---
+++ Assets/Packages/MeshBuilder/Scripts/Helpers/VertexHelper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace mattatz.MeshBuilderSystem {
+
+    [ExecuteInEditMode]
+    [RequireComponent (typeof(MeshFilter))]
+    public class VertexHelper : MonoBehaviour {
+
+        [SerializeField] float size = 0.025f;
+        Mesh mesh;
+
+        void Start () {
+            mesh = GetComponent<MeshFilter>().sharedMesh;
+        }
+
+        void OnDrawGizmos () {
+            if (mesh == null) return;
+
+            Gizmos.matrix = transform.localToWorldMatrix;
+
+            var vertices = mesh.vertices;
+            var uvs = mesh.uv;
+            for(int i = 0, n = vertices.Length; i < n; i++) {
+                var v = vertices[i];
+                var uv = uvs[i];
+                Gizmos.color = new Color(uv.x, uv.y, 0f);
+                Gizmos.DrawSphere(v, size);
+            }
+        }
+
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,30 @@
+unity-mesh-builder
+=====================
+
+Primitive mesh builders for Unity.
+
+![Demo](https://raw.githubusercontent.com/mattatz/unity-mesh-builder/master/Captures/Demo.png)
+
+## Usage
+
+```cs
+Mesh mesh = PlaneBuilder(
+    5f,     // width
+    10f,    // height
+    2,      // width segments
+    4       // height segments
+);
+```
+
+## Primitives
+
+- Plane (Parametric)
+- Cube
+- Cylinder
+- Sphere
+
+Another primitives are in progress ...
+
+## Sources
+
+- ProceduralPrimitives - Unity Community Wiki - http://wiki.unity3d.com/index.php/ProceduralPrimitives
